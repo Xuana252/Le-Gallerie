7d1c7c2edcac001c62601707f4446b47a7dc6548
Xuana252 <notfuny4927@gmail.com>
2024-12-05 17:30:52 +0700

    debug error

--- actions/accountActions.ts
+++ actions/accountActions.ts
@@ -152,7 +152,7 @@ export const changeUserPassword = async (user: string, newpassword: string) => {
 export const fetchUsers = async (searchText:string) => {
     const response = await fetch(
       `${process.env.DOMAIN_NAME}/api/users?searchText=${searchText}`,{
-        headers: headers()
+        headers: new Headers(headers()),
       }
     );
     const data = await response.json();
