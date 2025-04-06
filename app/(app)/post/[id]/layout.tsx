c889f56798ad5aa01730a8d3d2de19bccf7f249b
Xuana252 <notfuny4927@gmail.com>
2025-04-06 14:55:57 +0700

    debug metadata for sharing

---
+++ app/(app)/post/[id]/layout.tsx
@@ -0,0 +1,42 @@
+import { fetchPostWithId } from "@actions/postActions";
+
+export async function generateMetadata({ params }: { params: { id: string } }) {
+    const data = await fetchPostWithId(params.id);
+  
+    if (data.status === 200 && data.data) {
+      const post = data.data;
+      return {
+        title: post.title,
+        openGraph: {
+          title: post.title,
+          description: "Check out this amazing photo!",
+          images: [
+            {
+              url: post.image[0],
+              width: 1200,
+              height: 630,
+              alt: post.title,
+            },
+          ],
+          url: `${process.env.NEXT_PUBLIC_DOMAIN_NAME}/post/${post._id}`,
+          type: "website",
+        },
+      };
+    }
+  
+    return {
+      title: "Post Not Found",
+      openGraph: {
+        title: "Post Not Found",
+        description: "This post is unavailable.",
+      },
+    };
+  }
+
+export default function SettingLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <div>{children}</div>;
+}
